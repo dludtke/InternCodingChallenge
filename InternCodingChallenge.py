ff899b8661ef11af70c331da7bff464aa88b7332
Danielle Ludtke <dludtke@madisoncollege.edu>
2020-11-14 19:47:42 -0600

    Finished cleaning up - everything is more visually appealing now, as well as functional.

--- InternCodingChallenge.py
+++ InternCodingChallenge.py
@@ -2,23 +2,23 @@
 import requests
 import sys
 
-# Set the request parameters
+# Request parameters
 url = 'https://dludtke.zendesk.com/api/v2/tickets.json?page[size]=25'
-user = input("Enter your username: ")
-pwd = input("Enter your password: ")
-#id = input("Enter ticket ID (optional): ")
-#print(type(id))
+user = input("Please enter your username: ")
+pwd = input("Please enter your password: ")
 
-print("Welcome to the CLI ticket viewer!")
-print("To view all tickets, press 0.")
+# User options
+print("\nWelcome to the CLI ticket viewer!\n")
+print("To view all tickets, please press enter.")
 print("To view a single ticket, please specify the ticket ID.")
-print("To exit, press x")
-userselection = input("")
+print("To exit, please press x.\n")
+userselection = input(": ")
 
-if userselection == "0":
-    print("0 entered")
+# Print all tickets
+if userselection == "":
+    print("\nPrinting all tickets.\n")
     while url:
-        # Do the HTTP get request
+        # HTTP get request
         response = requests.get(url, auth=(user, pwd))
 
         # Check for HTTP codes other than 200
@@ -36,11 +36,13 @@ if userselection == "0":
             url = data['links']['next']
         else:
             url = None
+# Exit the CLI ticket viewer
 elif userselection == "x":
-    print("Goodbye!")
+    print("\nExiting. Goodbye!\n")
     sys.exit()
+# Print the user selected ticket
 else:
-    print("Not x or 0")
+    print("\nTicket ID requsted\n.")
     # Do the HTTP get request
     url = 'https://dludtke.zendesk.com/api/v2/tickets/'+userselection+'.json'
     print(url)
@@ -53,20 +55,5 @@ else:
 
     # Decode the JSON response into a dictionary and use the data
     data = response.json()
-    #print(data)
+    
     print("ID: ",data['ticket']['id']," SUBJECT: ",data['ticket']['subject'],"\nCONTENT: \n",data['ticket']['description'])
-
-
-# Parse tickets
-#if id == "": 
-#    for ticket in ticket_list:
-#        print("ID: ",ticket['id']," SUBJECT: ",ticket['subject'])
-#else:
-    #print("ID: ",id," SUBJECT: ",ticket_list[id]['subject'])
-#    for ticket in ticket_list:
-#        if ticket['id'] == int(id):
-#            print("ID: ",ticket['id']," SUBJECT: ",ticket['subject'],"CONTENT: ",ticket['description'])
-#if data['meta']['has_more']:
-#    url = data['links']['next']
-#else:
-#    url = None
